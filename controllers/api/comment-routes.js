d973adc87bb8077334a0bee11c5a1e9dc36deebf
Alex Bright <abrighttt@gmail.com>
2021-07-30 01:37:40 -0400

    files modified

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+const userRoutes = require('./user-routes.js');
+
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;
---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes');
+
+router.use('/post', postRoutes);
+router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
